5e6b85d6f05978d22b563816f6a071b750396bc3
yuadac <44121256+yuadac@users.noreply.github.com>
2018-10-14 16:31:37 +0900

    Add files via upload

---
+++ Models/AzureStorageConfig.cs
@@ -0,0 +1,16 @@
+namespace FileUploader.Models
+{
+    public class AzureStorageConfig
+    {
+        public AzureStorageConfig()
+        {
+            //ConnectionString = "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=antenasa;AccountKey=gI061dKPINCEeYjyyMlZtPEJGg8hBiMn5iHL5OovCliU0VedFl1+c25Q5oxWpuLIZMo4qr/1z491pVogDlpM0w==";
+            //FileContainerName = "files";
+            //AnalyzedFileContainerName = "analyzed";
+        }
+
+        public string ConnectionString { get; set; }
+        public string FileContainerName { get; set; }
+        public string AnalyzedFileContainerName { get; set; }
+    }
+}
